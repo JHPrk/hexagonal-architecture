24b50608839486e09e5d0b29baf0bcf97b2d498f
JHPrk <qkrwogus1236@gmail.com>
2025-09-28 19:40:40 +0900

    NaturalId, NaturalIdCache 로 자연키 제약조건 해결

--- src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package tobyspring.splearn.application.required;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static tobyspring.splearn.domain.MemberFixture.createMemberRegisterRequest;
 import static tobyspring.splearn.domain.MemberFixture.createPasswordEncoder;
@@ -8,6 +9,7 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import tobyspring.splearn.domain.Member;
 
 
@@ -30,5 +32,15 @@ class MemberRepositoryTest {
     assertThat(member.getId()).isNotNull();
 
     entityManager.flush();
+  }
+
+  @Test
+  void duplicateEmailFail() {
+    Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+    memberRepository.save(member);
+
+    Member member2 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+    assertThatThrownBy(() -> memberRepository.save(member2))
+        .isInstanceOf(DataIntegrityViolationException.class);
   }
 }
