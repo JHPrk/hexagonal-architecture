4ead466b7185e5e089b5385f40c2dc5b457bf2c3
JHPrk <qkrwogus1236@gmail.com>
2025-09-28 16:24:18 +0900

    Application 레이어 포트 정의

---
+++ src/main/java/tobyspring/splearn/application/required/MemberRepository.java
@@ -0,0 +1,12 @@
+package tobyspring.splearn.application.required;
+
+import org.springframework.data.repository.Repository;
+import tobyspring.splearn.domain.Member;
+
+/**
+ * 회원 정보를 저장하거나 조회한다.
+ * 복잡해지면 읽기용 쓰기용 분리할 수 있음
+ */
+public interface MemberRepository extends Repository<Member, Long> {
+  Member save(Member member);
+}
