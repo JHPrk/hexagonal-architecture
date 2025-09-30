2b315fafef89c7550a14660e7609235cb805ec0f
JHPrk <qkrwogus1236@gmail.com>
2025-10-01 01:01:50 +0900

    애플리케이션 회원 등록 테스트 코드 작성

---
+++ src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
@@ -0,0 +1,38 @@
+package tobyspring.splearn.application.provided;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import tobyspring.splearn.SplearnTestConfiguration;
+import tobyspring.splearn.domain.DuplicateEmailException;
+import tobyspring.splearn.domain.Member;
+import tobyspring.splearn.domain.MemberFixture;
+import tobyspring.splearn.domain.MemberStatus;
+
+@SpringBootTest
+@Transactional
+@Import(SplearnTestConfiguration.class)
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+public record MemberRegisterTest(MemberRegister memberRegister) {
+
+  @Test
+  void register() {
+    Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+    assertThat(member.getId()).isNotNull();
+    assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+  }
+
+  @Test
+  void duplicateEmailFail() {
+    Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+    assertThatThrownBy(() -> memberRegister.register(MemberFixture.createMemberRegisterRequest()))
+        .isInstanceOf(DuplicateEmailException.class);
+  }
+}
