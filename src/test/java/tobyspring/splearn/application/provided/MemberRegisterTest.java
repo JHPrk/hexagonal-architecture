3bbd0fa46628827d2b6f981c15a41c64788cffef
JHPrk <qkrwogus1236@gmail.com>
2025-10-01 23:54:36 +0900

    validation JSR-303 표준 기능 이용해서 요청 검증 로직 추가

--- src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
+++ src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
@@ -2,7 +2,10 @@ package tobyspring.splearn.application.provided;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static tobyspring.splearn.domain.MemberFixture.createMemberRegisterRequest;
+import static tobyspring.splearn.domain.MemberFixture.createPasswordEncoder;
 
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -12,6 +15,7 @@ import tobyspring.splearn.SplearnTestConfiguration;
 import tobyspring.splearn.domain.DuplicateEmailException;
 import tobyspring.splearn.domain.Member;
 import tobyspring.splearn.domain.MemberFixture;
+import tobyspring.splearn.domain.MemberRegisterRequest;
 import tobyspring.splearn.domain.MemberStatus;
 
 @SpringBootTest
@@ -34,5 +38,20 @@ public record MemberRegisterTest(MemberRegister memberRegister) {
 
     assertThatThrownBy(() -> memberRegister.register(MemberFixture.createMemberRegisterRequest()))
         .isInstanceOf(DuplicateEmailException.class);
+  }
+
+
+
+  @Test
+  void memberRegisterRequestFail() {
+    invalidRequest(new MemberRegisterRequest("toby@spelearn.app", "Toby", "longsecret"));
+    invalidRequest(new MemberRegisterRequest("toby@spelearn.app", "Charlie___________________________________________", "longsecret"));
+    invalidRequest(new MemberRegisterRequest("tobyspelearn.app", "Charlie", "longsecret"));
+    invalidRequest(new MemberRegisterRequest("toby@spelearn.app", "Charlie", "short"));
+  }
+
+  private void invalidRequest(MemberRegisterRequest invalid) {
+    assertThatThrownBy(() -> memberRegister.register(invalid))
+        .isInstanceOf(ConstraintViolationException.class);
   }
 }
